fd79033395d2b92c09134f6d8818f9ea3fcc3ac8
Lucas Prini <nagix@nagixtecnologia.com.br>
2026-09-16 15:38:51 -0300

    Enable EPEL repository before installing troubleshooting tools

--- terraform/windows-deploy/main.tf
+++ terraform/windows-deploy/main.tf
@@ -1,17 +1,3 @@
-variable "pve_endpoint" { type = string }
-variable "pve_api_token" { type = string; sensitive = true }
-variable "vm_hostname" { type = string }
-variable "ip_mode" { type = string; default = "dhcp" }
-variable "vm_ip" { type = string; default = "" }
-variable "vm_gateway" { type = string; default = "" }
-variable "template_vm_id" { type = number; default = 128 }
-variable "node_name" { type = string; default = "host-pve-nagixdtc02" }
-variable "storage_id" { type = string; default = "local-lvm" }
-variable "cpu_cores" { type = number; default = 4 }
-variable "memory_mb" { type = number; default = 4096 }
-variable "disk_size_gb" { type = number; default = 60 }
-variable "admin_password" { type = string; sensitive = true }
-
 locals {
   vm_name = "pve-${var.vm_hostname}"
 }
